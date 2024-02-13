138a00bef0f7368c329099daefc1a14eecd9b849
Jalumnefp <jauferper@alu.edu.gva.es>
2024-02-13 17:58:47 +0100

    update model

--- ArtApi/models.py
+++ ArtApi/models.py
@@ -6,4 +6,4 @@ from django.db.models import Model, CharField, ImageField, TextField
 class Artwork(Model):
     title = CharField(max_length=50)
     author = CharField(max_length=50)
-    image = TextField()
+    image = TextField(max_length=1000)
